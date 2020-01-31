60819cbb4743e81cbfe2969858cd267a713a97d5
Walter Porfirio Silveira <walter.silveira@linx.com.br>
2020-01-31 17:55:57 -0300

    css aplicado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+//Componente: Bloco isolado de HTML, CSS e JS, que não interfere no resto da aplicação
+//Propriedade: Informações que um componente PAI passa para o componente FILHO
+//Estado: Informações mantidas pelo componente (imutabilidade)
 import React from 'react';
+import './global.css'
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <div id="app">
+
+    </div>
   );
 }
 
